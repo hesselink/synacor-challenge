c96bae7cb625bfde5e65aa58501fffc5736874ce
Erik Hesselink <ehesselink@palantir.com>
2020-02-13 22:23:04 +0100

    Remove commented out code.

--- src/OpCode.hs
+++ src/OpCode.hs
@@ -1,30 +1,5 @@
 module OpCode where
 
--- data Op
---   = Halt
---   | Set Reg Val
---   | Push Val
---   | Pop Val
---   | Eq Val Val Val
---   | Gt Val Val Val
---   | Jmp Val
---   | Jt Val Val
---   | Jf Val Val
---   | Add Val Val Val
---   | Mult Val Val Val
---   | Mod Val Val Val
---   | And Val Val Val
---   | Or Val Val Val
---   | Not Val Val
---   | RMem Val Val
---   | Wmem Val Val
---   | Call Val
---   | Ret
---   | Out Val
---   | In Val
---   | Noop
---   deriving (Show, Eq)
- 
 data OpCode
   = Halt
   | Set
