07a72e4e2c172243f2f6d672d2d0a0e7225c953c
Erik Hesselink <ehesselink@palantir.com>
2020-02-14 19:11:30 +0100

    Add multiplication

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -112,6 +112,11 @@ runOp cd = case cd of
     v1 <- readVal
     v2 <- readVal
     writeVal target ((v1 + v2) `mod` 32768)
+  Mult -> do
+    target <- readAddr
+    v1 <- readVal
+    v2 <- readVal
+    writeVal target ((v1 * v2) `mod` 32768)
   And -> do
     target <- readAddr
     v1 <- readVal
--- test/Tests.hs
+++ test/Tests.hs
@@ -24,6 +24,7 @@ tests = testGroup "Tests" $
   , testProperty "JumpIfTrue" testJumpIfTrue
   , testProperty "JumpIfFalse" testJumpIfFalse
   , testProperty "Addition" testAddition
+  , testProperty "Multiplication" testMultiplication
   , testProperty "And" testAnd
   , testProperty "Or" testOr
   , testProperty "Not" testNot
@@ -109,6 +110,17 @@ testAddition x y target source1 source2 = target /= source1 && target /= source2
         Mem l -> Val l
         Reg _ -> y
   in runAndCheckEquals target ((v1 + v2) `mod` 32768) st
+
+testMultiplication :: Val -> Val -> Addr -> Addr -> Addr -> Property
+testMultiplication x y target source1 source2 = target /= source1 && target /= source2 && source1 /= source2 ==>
+  let st = addProgram [Mult target source1 source2] . setAt source1 x . setAt source2 y $ emptyState
+      v1 = case source1 of
+        Mem l -> Val l
+        Reg _ -> x
+      v2 = case source2 of
+        Mem l -> Val l
+        Reg _ -> y
+  in runAndCheckEquals target ((v1 * v2) `mod` 32768) st
 
 testAnd :: Val -> Val -> Addr -> Addr -> Addr -> Property
 testAnd x y target source1 source2 = target /= source1 && target /= source2 && source1 /= source2 ==>
@@ -210,6 +222,7 @@ serializeOp op = case op of
   Jt a b -> 7 : map serializeAddr [a, b]
   Jf a b -> 8 : map serializeAddr [a, b]
   Add a b c -> 9 : map serializeAddr [a, b, c]
+  Mult a b c -> 10 : map serializeAddr [a, b, c]
   -- TODO
   And a b c -> 12 : map serializeAddr [a, b, c]
   Or a b c -> 13 : map serializeAddr [a, b, c]
